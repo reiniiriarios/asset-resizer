a9460c8361575d8d3ebec1d427fb33c8f411a5cf
Emma Litwa-Vulcu <80431903+reiniiriarios@users.noreply.github.com>
2024-04-17 18:40:01 -0700

    feat: progress bar

--- src/log.ts
+++ src/log.ts
@@ -2,6 +2,8 @@ import chalk from "chalk";
 
 const DEBUG_MODE = process.env.ASSET_RESIZE_DEBUG === "true";
 
+const PROGRESS_WIDTH = 80;
+
 export function log(any: unknown) {
   let out: string;
   if (typeof any === "object") {
@@ -22,4 +24,11 @@ export function debug(any: unknown) {
   if (DEBUG_MODE) {
     log(any);
   }
+}
+
+export function progress(position: number, max: number) {
+  const percent = position / max;
+  const pips = Math.round(percent * (PROGRESS_WIDTH - 2));
+  const spaces = PROGRESS_WIDTH - 2 - pips;
+  process.stdout.write(`\r[${"#".repeat(pips)}${" ".repeat(spaces)}]`);
 }
--- src/process.ts
+++ src/process.ts
@@ -3,7 +3,8 @@ import path from "path";
 import { AssetResizerAsset, AssetResizerConfig, AssetResizerOutput } from "./types.js";
 import { loadConfig } from "./config.js";
 import sharp from "sharp";
-import { err } from "./log.js";
+import { err, log, progress } from "./log.js";
+import chalk from "chalk";
 
 export async function parseAllAssets(config?: AssetResizerConfig) {
   if (!config) {
@@ -32,15 +33,23 @@ export async function parseAllAssets(config?: AssetResizerConfig) {
     return;
   }
 
+  const numAssets = config.assets.length;
+  const numOutputs = config.assets.reduce((n, c) => n + c.output?.length ?? 0, 0);
+  let numOutputsParsed = 0;
+
+  log("Processing...");
   parseEachAsset(config.assets);
 
   async function parseEachAsset(assets: AssetResizerAsset[]) {
     for (const asset of assets) {
       const assetPath = path.join(inputDir, asset.file);
       for (const output of asset.output) {
+        progress(numOutputsParsed, numOutputs);
         await parseAssetOutput(assetPath, output);
+        numOutputsParsed++;
       }
     }
+    log(`\r${chalk.green("Done!")} Processed: ${numAssets} input assets, ${numOutputs} created.`.padEnd(91, " "));
   }
 
   async function parseAssetOutput(assetPath: string, output: AssetResizerOutput) {
@@ -52,6 +61,9 @@ export async function parseAllAssets(config?: AssetResizerConfig) {
     await sharp(assetPath)
       .resize(output.width, output.height ?? output.width, { fit: output.fit ?? "inside" })
       .toFile(outputPath)
-      .catch((e) => err(e));
+      .catch((e) => {
+        log("\n");
+        err(e);
+      });
   }
 }
